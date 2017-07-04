84d02d94ea5988f292f3dbaa8f268f27c50cdc3e
Jeane Carlos <volbohel@gmail.com>
2017-07-04 15:24:13 -0400

    V 0.10

---
+++ jPokedex/PokeCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCollectionViewCell.swift
+//  jPokedex
+//
+//  Created by Jeane Carlos on 7/4/17.
+//  Copyright © 2017 Jeane Carlos. All rights reserved.
+//
+
+import UIKit
+
+class PokeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+}
--- jPokedex/README.md
+++ jPokedex/README.md
@@ -3,3 +3,4 @@ This is a Pokedex app using the Pokemon API which can be found on https://pokeap
 
 V 0.00 First Commit
 V 0.05 Initial UI Planning is set up (Search bar, UICollectionview, Imageviews)
+V 0.10 UI Now Displays all 700+ Pokemon
