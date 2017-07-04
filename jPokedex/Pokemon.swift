81b50e9922ce7c3265a8b4f2a186945dd592258b
Jeane Carlos <volbohel@gmail.com>
2017-07-04 15:00:00 -0400

    V 0.05

---
+++ jPokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  jPokedex
+//
+//  Created by Jeane Carlos on 7/4/17.
+//  Copyright © 2017 Jeane Carlos. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
---
+++ jPokedex/README.md
@@ -0,0 +1,5 @@
+This is a Pokedex app using the Pokemon API which can be found on https://pokeapi.co/
+
+
+V 0.00 First Commit
+V 0.05 Initial UI Planning is set up (Search bar, UICollectionview, Imageviews)
